e3be3242805c6febc5e695b1269c31ce2b6fa7e1
Zineb Elouarradi <58147119+zinebelouarradi@users.noreply.github.com>
2020-10-17 16:23:09 +0100

    Add files via upload

---
+++ precache-manifest.6bbaa88be64b5233e060eafbc498ae38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04aa98689806eeb80c295db8776b8f45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9685a828475f0434f6e1",
+    "url": "/static/css/main.a398fce6.chunk.css"
+  },
+  {
+    "revision": "7114260f8230b215af12",
+    "url": "/static/js/2.39466f51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39466f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9685a828475f0434f6e1",
+    "url": "/static/js/main.fc15080d.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
